dddce60ac286608f75373d35180da460bd3efe4c
Nick Birgfeld <nickbirgfeld676@gmail.com>
2023-10-01 18:03:25 +0200

    remove old code

--- js/navbar.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("scroll", () => {
-    let navbar = document.getElementsByClassName("navbar")[0];
-    if (scrollY > 1) {
-        navbar.classList.add("navbar-scroll")
-    } else
-        navbar.classList.remove("navbar-scroll")
-})
